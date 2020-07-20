3ff09ba47b655830728ac9933fd9cadf48ae1295
ColonelKai <59742334+ColonelKai@users.noreply.github.com>
2020-07-20 20:40:04 +0300

    Test v.Alpha.0.3

--- src/main/java/micronesiadevelopment/microstates/StateCommandHandler.java
+++ src/main/java/micronesiadevelopment/microstates/StateCommandHandler.java
@@ -22,25 +22,34 @@ public class StateCommandHandler implements CommandExecutor {
         //this is where the suffering begins.
         //piece of code that will create and save a state.
         else if(args[0].equals("create")){
-            if(!args[1].isEmpty() && !args[2].isEmpty()){
-                //Create Stuff. I guess. I dont fucking know.
+            if(args.length == 3){
+                //Create Stuff. I guess. I don't fucking know.
             	String state_name = args[1];
             	State newstate = new State();
-            	
-            	if (!State.stateExsistsOnDisk(state_name)) {
-                    Player owner = Bukkit.getPlayerExact(args[2]);
 
-                    //Hehe lets get this boi going. This sets up some values that needs to be set.
-                    newstate.setOwner(owner.getUniqueId());
-                    newstate.changeName(state_name);
-                    newstate.setUpState();
-            	} else {
-            		newstate.getFromFile(state_name);
-            	}
-                newstate.save();
+                if(sender.hasPermission("states.admin")){
+                    if (!State.stateExsistsOnDisk(state_name)) {
+                        Player owner = Bukkit.getPlayerExact(args[2]);
 
+                        //Hehe lets get this boi going. This sets up some values that needs to be set.
+                        newstate.setOwner(owner.getUniqueId());
+                        newstate.changeName(state_name);
+                        newstate.setUpState();
+                        sender.sendMessage(ChatColor.BLUE + "[MicroStates]" + ChatColor.WHITE + "Registered State: " + state_name);
+                    } else {
+                        newstate.getFromFile(state_name);
+                        sender.sendMessage(ChatColor.BLUE + "[MicroStates]" + ChatColor.RED + "A state with that name already exists.");
+                    }
+                    newstate.save();
+                } else {
+                    sender.sendMessage(ChatColor.BLUE + "[MicroStates]" + ChatColor.RED + "You dont have permission to do that.");
+                }
+
+            } else {
+                sender.sendMessage(ChatColor.BLUE + "[MicroStates]" + ChatColor.RED + "Usage: /state create [state name] [owner]");
+                }
             }
-        }
+
 
         else{sender.sendMessage(ChatColor.BLUE + "[MicroStates]" + ChatColor.RED + " Unknown command, /state help for help.");}
 
